0af92638df19348f99e386d820a650d173d07f2d
harold <yamada.yeshua@gmail.com>
2024-07-19 20:48:19 -0600

    Inicio del dashboard y problemas con conectar con el css

--- dashboard.php
+++ dashboard.php
@@ -1,8 +1,5 @@
 <?php
 session_start();
-
-
-
 // Verificar si el usuario está autenticado
 if (!isset($_SESSION['email'])) {
     // Si no está autenticado, redirigir al inicio de sesión
@@ -19,10 +16,11 @@ if (!isset($_SESSION['email'])) {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="css/dashboard.css">
-  <script src="https://kit.fontawesome.com/ae360af17e.js" crossorigin="anonymous"></script>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined">
   <title>Dashboard</title>
 </head>
+
+
 
 
 <body>
@@ -30,27 +28,52 @@ if (!isset($_SESSION['email'])) {
   <p>Contenido del dashboard...</p>
 
   <a href="logout.php">Cerrar sesión</a>
+  <span class="material-symbols-outlined">
+close
+</span>
 
-  <div class="wrapper">
-    <aside id="sidebar">
+<!--Navegador -->
+  <div class="container">
+    <aside>
+
+      <div class="top">
+        <div class="logo">
+          <img src="img/lars_limpio.png" alt="">
+          <h2>LA<span class="succeses">RS</span></h2>
+        </div>
+
+        <div class="close" id="close-btn">
+          <span class="material-symbols-outlined">close</span>
+        </div>
+      </div>
+
+      <div class="sidebar">
+
+        <a href="#">
+          <span class="material-symbols-outlined">space_dashboard</span>
+          <h3>Inicio</h3>
+        </a>
+        <a href="#">
+          <span class="material-symbols-outlined">group</span>
+          <h3>Grados</h3>
+        </a>
+        <a href="#">
+          <span class="material-symbols-outlined">collections_bookmark</span>
+          <h3>Materias</h3>
+        </a>
+        <a href="#">
+          <span class="material-symbols-outlined">school</span>
+          <h3>Totales</h3>
+        </a>
+
+      </div>
 
     </aside>
-
-    <section class="main">
-      <mav class="navbar">
-        <button class="btn">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-      </mav>
-    </section>
-
   </div>
 
 
 
 
   <script src="js/dashboard.js" ></script>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
 </body>
 </html>
